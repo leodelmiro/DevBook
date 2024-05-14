a7f0b46bbf90471211aecdc9b5d2bab9034f441f
Leonardo Delmiro <leodelmiroms@gmail.com>
2024-05-14 14:22:36 -0300

    feat: routes

---
+++ api/src/controllers/users.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "net/http"
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating User!"))
+}
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Getting Users!"))
+}
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Getting User!"))
+}
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Updating User!"))
+}
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deleting User!"))
+}
---
+++ api/src/router/routes/users.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var routesUsers = []Route{
+	{
+		URI:           "/users",
+		Method:        http.MethodPost,
+		Function:      controllers.CreateUser,
+		RequerireAuth: false,
+	},
+	{
+		URI:           "/users",
+		Method:        http.MethodGet,
+		Function:      controllers.GetUsers,
+		RequerireAuth: false,
+	},
+	{
+		URI:           "/users/{userId}",
+		Method:        http.MethodGet,
+		Function:      controllers.GetUser,
+		RequerireAuth: false,
+	},
+	{
+		URI:           "/users/{userId}",
+		Method:        http.MethodPut,
+		Function:      controllers.UpdateUser,
+		RequerireAuth: false,
+	},
+	{
+		URI:           "/users/{userId}",
+		Method:        http.MethodDelete,
+		Function:      controllers.DeleteUser,
+		RequerireAuth: false,
+	},
+}
